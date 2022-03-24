095cdf0c5ff9b5a0338cf4407342bded49cdf363
aiotter <git@aiotter.com>
2022-03-24 20:37:55 +0900

    chore(test): Better test

--- test/react-compat.test.tsx
+++ test/react-compat.test.tsx
@@ -13,9 +13,13 @@ Deno.test(function simpleRSS() {
             <title>item0</title>
             <link>http://example.com/item0/</link>
           </item>
+          <item>
+            <title>item1</title>
+            <link>http://example.com/item1/</link>
+          </item>
         </channel>
       </rss>,
     ),
-    '<rss version="2.0"><channel><title>Channel Title</title><link>http://example.com</link><description>Channel Description</description><item><title>item0</title><link>http://example.com/item0/</link></item></channel></rss>',
+    '<rss version="2.0"><channel><title>Channel Title</title><link>http://example.com</link><description>Channel Description</description><item><title>item0</title><link>http://example.com/item0/</link></item><item><title>item1</title><link>http://example.com/item1/</link></item></channel></rss>',
   );
 });
